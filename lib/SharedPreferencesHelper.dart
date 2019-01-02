e565de97b0ce90401782d987fef780b8945984c9
Laco <couturier.laurent75@gmail.com>
2019-01-02 22:32:53 +0100

    Refactoring

--- lib/SharedPreferencesHelper.dart
+++ lib/SharedPreferencesHelper.dart
@@ -1,11 +1,9 @@
-
 import 'package:shared_preferences/shared_preferences.dart';
 
-
-class SharedPreferencesHelper {
+class Preferences {
   static final String _kLanguageCode = "language";
 
-  static Future<String> getLanguageCode() async {    
+  static Future<String> getLanguageCode() async {
     return getValue(_kLanguageCode) ?? "fr";
   }
 
@@ -14,7 +12,7 @@ class SharedPreferencesHelper {
   }
 
   static Future<String> getValue(String key) async {
-  if (key == null){
+    if (key == null) {
       throw new ArgumentError("key");
     }
     final SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -22,11 +20,22 @@ class SharedPreferencesHelper {
     return prefs.getString(key) ?? null;
   }
 
-  static Future<bool> setValue(String key, String value) async {
-    if (key == null){
+  static Future<T> getValueOrDefault<T>(
+      String key, T Function() operation) async {
+    if (key == null) {
       throw new ArgumentError("key");
     }
-    if (value == null){
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    bool exist = prefs.get(key) != null;
+    return exist ? prefs.get(key) as T : operation();
+  }
+
+  static Future<bool> setValue(String key, String value) async {
+    if (key == null) {
+      throw new ArgumentError("key");
+    }
+    if (value == null) {
       throw new ArgumentError("value");
     }
 
@@ -34,16 +43,22 @@ class SharedPreferencesHelper {
 
     return prefs.setString(key, value);
   }
+
   static Future<bool> setBool(String key, bool value) async {
-    if (key == null){
+    if (key == null) {
       throw new ArgumentError("key");
     }
-    if (value == null){
+    if (value == null) {
       throw new ArgumentError("value");
     }
 
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-
     return prefs.setBool(key, value);
+  }
+
+  static void clearAll() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    ["Lower", "Upper", "Number", "Size"].forEach((x) => prefs.remove(x));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,8 @@ import 'package:password_generator/Translations.dart';
 
 void main() {
   runApp(new MaterialApp(
-    localizationsDelegates: [   
-      const TranslationsDelegate(),  
+    localizationsDelegates: [
+      const TranslationsDelegate(),
       GlobalMaterialLocalizations.delegate,
       GlobalWidgetsLocalizations.delegate,
     ],
@@ -21,11 +21,11 @@ void main() {
     ],
     home: new SplashScreen(),
     routes: <String, WidgetBuilder>{
-      '/HomeScreen': (BuildContext context) => new MyHomePage(title: "Générateur de mot de passe")
+      '/HomeScreen': (BuildContext context) =>
+          new MyHomePage(title: "Générateur de mot de passe")
     },
   ));
 }
-
 
 @Deprecated('Old App')
 class MyApp extends StatelessWidget {
@@ -33,19 +33,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: Translations.of(context).text("app_title"),      
-      theme: ThemeData(        
-        primarySwatch: Colors.blue,
-      ),
-      //home: MyHomePage(title: Translations.of(context).text("main_title")),
-      home: MyHomePage(title: "Password Generator")
-    );
+        title: Translations.of(context).text("app_title"),
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: Translations.of(context).text("main_title")));
   }
 }
-  
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
- 
+
   final String title;
 
   @override
@@ -59,85 +57,102 @@ class _MyHomePageState extends State<MyHomePage> {
   bool _number = false;
   String _password = "";
 
+  @override
+  void initState() {
+    super.initState();
+    // TODO : Load SharedPreferences
+  }
+
   void _setSize(double value) {
-    setState(() {     
-     _size = value.toInt();
+    setState(() {
+      _size = value.toInt();
     });
   }
 
-  void _computePassword() {
-    setState(() {     
-      _password = Password.of(_size, CharType.upper | CharType.lower | CharType.number);
+  void _generatePassword() {
+    setState(() {
+      CharType value = CharType.none;
+      if (_lower) {
+        value |= CharType.lower;
+      }
+      if (_upper) {
+        value |= CharType.upper;
+      }
+      if (_number) {
+        value |= CharType.number;
+      }
+      _password = Password.of(_size, value);
     });
   }
 
   @override
-  Widget build(BuildContext context) {    
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(        
+      appBar: AppBar(
         title: Text(widget.title),
         actions: <Widget>[
-          PopupMenuButton<String>(            
-            onSelected: choiceAction, 
+          PopupMenuButton<String>(
+            onSelected: choiceAction,
             itemBuilder: getPopMenuEntry,
-          )           
-        ],        
+          )
+        ],
       ),
-      body: Center(        
-        child: Column(          
+      body: Center(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[     
+          children: <Widget>[
             Text(
               '$_size caractères',
               style: Theme.of(context).textTheme.display1,
             ),
-            Slider(              
+            Slider(
               label: "Nombre de caractères",
               min: 4,
-              max: 64, 
-              value: _size.roundToDouble(), 
+              max: 64,
+              value: _size.roundToDouble(),
               onChanged: _setSize,
             ),
             Row(
-              mainAxisAlignment:  MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
-              Text("Minuscules"),
-              Switch(
-              value: false,     
-              key: Key("lowerSwitch"), 
-              onChanged: (bool value) {
-                _lower = value;
-              },                  
-              ),     
-              ],            
+                Text("Minuscules"),
+                Switch(
+                  value: _lower,
+                  key: Key("lowerSwitch"),
+                  onChanged: (bool value) {
+                    _lower = value;
+                  },
+                ),
+              ],
             ),
             Row(
-              mainAxisAlignment:  MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
-              Text("Majuscules", ),
-              Switch(
-              value: false, 
-              onChanged: (bool value) {
-                _upper = value;
-              },         
-              key: Key("upperSwitch"),                  
-              ),     
-
-              ],            
-            ),     
+                Text(
+                  "Majuscules",
+                ),
+                Switch(
+                  value: _upper,
+                  onChanged: (bool value) {
+                    _upper = value;
+                  },
+                  key: Key("upperSwitch"),
+                ),
+              ],
+            ),
             Row(
-              mainAxisAlignment:  MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
-              Text("Nombres"),
-              Switch(
-              value: false, 
-              onChanged: (bool value) {
-                _number = value;
-              },         
-              key: Key("numberSwitch"),                  
-              ),     
-              ],            
-            ),                   
+                Text("Nombres"),
+                Switch(
+                  value: _number,
+                  onChanged: (bool value) {
+                    _number = value;
+                  },
+                  key: Key("numberSwitch"),
+                ),
+              ],
+            ),
             Text(
               '$_password',
               style: Theme.of(context).textTheme.display1,
@@ -146,58 +161,47 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _computePassword,      
+        onPressed: _generatePassword,
         tooltip: 'Génération du mot de passe',
         child: Icon(Icons.done),
-      ), 
+      ),
     );
   }
-
-
 
   void choiceAction(String choice) {
-      // TODO : Implémenter le traitement des commandes
-      if (choice == "Copy")
-      {
-        ClipboardManager.copyToClipBoard("Mot de passe généré...");
-      }
-      if (choice == "Save")
-      {
-          _saveOptions();
-      }
+    if (choice == "Copy") {
+      ClipboardManager.copyToClipBoard(_password);
+    }
+    if (choice == "Reinit") {
+      Preferences.clearAll();
+    }
+    if (choice == "Save") {
+      _saveOptions();
+    }
   }
+
+  void _loadOptions() {}
 
   void _saveOptions() {
-    SharedPreferencesHelper.setBool("Lower", _lower);
-    SharedPreferencesHelper.setBool("Upper", _upper);
-    SharedPreferencesHelper.setBool("Number", _number);
-    SharedPreferencesHelper.setValue("Size", _size.toInt().toString());
+    Preferences.setBool("Lower", _lower);
+    Preferences.setBool("Upper", _upper);
+    Preferences.setBool("Number", _number);
+    Preferences.setValue("Size", _size.toInt().toString());
   }
 
-  PopupMenuItem<String> getPopUpItem(String choice){
-    return PopupMenuItem<String>(
-      value: choice,
-      child: Text(choice)
-    );
+  PopupMenuItem<String> getPopUpItem(String choice) {
+    return PopupMenuItem<String>(value: choice, child: Text(choice));
   }
 
-  List<PopupMenuEntry<String>> getPopMenuEntry(BuildContext context)
-  {
-    return ItemsPopup.Items.map(getPopUpItem).toList(); 
+  List<PopupMenuEntry<String>> getPopMenuEntry(BuildContext context) {
+    return ItemsPopup.Items.map(getPopUpItem).toList();
   }
 }
-
-
 
 class ItemsPopup {
   static const String Apparence = "Apparence";
   static const String Copy = "Copy";
-  static const List<String> Items = <String>[
-      Settings,
-      Copy, 
-      Apparence,
-      Save
-  ];
+  static const List<String> Items = <String>[Settings, Copy, Apparence, Save];
 
   static const String Save = "Save";
   static const String Settings = "Settings";
